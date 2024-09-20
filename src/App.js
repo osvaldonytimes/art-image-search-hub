f10e69cc08664200d568bec70527e28b186531fa
Osvaldo Concepcion <106048886+osvaldonytimes@users.noreply.github.com>
2024-09-20 14:50:18 -0400

    Initial routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
+import Navbar from "./components/Navbar";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar /> {/* Add the Navbar here */}
+        <Routes>
+          {/* Home (Root) */}
+          <Route path="/" element={<Home />} />
+
+          {/* Search */}
+          <Route path="/search" element={<Search />} />
+
+          {/* Saved */}
+          <Route path="/saved" element={<Saved />} />
+
+          {/* Redirect any unknown paths to root */}
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/search">Search</Link>
+        </li>
+        <li>
+          <Link to="/saved">Saved</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
