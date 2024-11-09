6cbc57aa352cf87d106d98aa343aad708dfb74db
Osvaldo Concepcion <106048886+osvaldonytimes@users.noreply.github.com>
2024-11-09 11:28:53 -0500

    Updating navigation bar styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { AppBar, Toolbar, Button } from "@mui/material";
+import { AppBar, Toolbar, Button, Box } from "@mui/material";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import { loginWithGoogle, logout } from "../firebase";
@@ -26,24 +26,44 @@ const Navbar = () => {
   };
 
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <Button color="inherit" component={Link} to="/">
-          Home
-        </Button>
-        <Button color="inherit" component={Link} to="/saved">
-          Saved
-        </Button>
+    <AppBar position="static" sx={{ bgcolor: "#F7F7F7", color: "#333333" }}>
+      <Toolbar sx={{ justifyContent: "center" }}>
+        <Box display="flex" gap={2}>
+          <Button
+            color="inherit"
+            component={Link}
+            to="/"
+            sx={{ fontWeight: "bold" }} // Make button text bold
+          >
+            Home
+          </Button>
+          <Button
+            color="inherit"
+            component={Link}
+            to="/saved"
+            sx={{ fontWeight: "bold" }} // Make button text bold
+          >
+            Saved
+          </Button>
 
-        {user ? (
-          <Button color="inherit" onClick={handleLogout}>
-            Logout
-          </Button>
-        ) : (
-          <Button color="inherit" onClick={handleLogin}>
-            Login with Google
-          </Button>
-        )}
+          {user ? (
+            <Button
+              color="inherit"
+              onClick={handleLogout}
+              sx={{ fontWeight: "bold" }} // Make button text bold
+            >
+              Logout
+            </Button>
+          ) : (
+            <Button
+              color="inherit"
+              onClick={handleLogin}
+              sx={{ fontWeight: "bold" }} // Make button text bold
+            >
+              Login with Google
+            </Button>
+          )}
+        </Box>
       </Toolbar>
     </AppBar>
   );
